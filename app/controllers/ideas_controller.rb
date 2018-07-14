26ef29e8b323340e78eb2118d5ebfa4155bd3047
Masa Kingica <tvitilova@gmail.com>
2018-07-14 14:41:21 +0200

    Comment

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,7 +1,9 @@
 class IdeasController < ApplicationController
   before_action :set_idea, only: [:show, :edit, :update, :destroy]
 
-  # GET /ideas
+
+  
+  # @comments = @idea.comments.all
   # GET /ideas.json
   def index
     @ideas = Idea.all
@@ -10,6 +12,8 @@ class IdeasController < ApplicationController
   # GET /ideas/1
   # GET /ideas/1.json
   def show
+    @comments = @idea.comments.all
+    @comment = @idea.comments.build
   end
 
   # GET /ideas/new
